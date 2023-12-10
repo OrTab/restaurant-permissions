5a9e62c84fa89db5f87606224d1aa2a812ad3a1f
OrTab <or.1532.tabin@gmail.com>
2023-12-10 02:24:42 +0200

    inital project , declare permissions

---
+++ permissions.h
@@ -0,0 +1,13 @@
+#ifndef PERMISSIONS
+
+#define READ_MENU 1 << 0
+#define EDIT_MENU 1 << 1
+#define PLACE_ORDERS 1 << 2
+#define MANAGE_RESERVATIONS 1 << 3
+#define VIEW_SALES_REPORTS 1 << 4
+#define PROCESS_PAYMENTS 1 << 5
+#define ACCESS_KITCHEN 1 << 6
+#define EMPLOYEE_MANAGEMENT 1 << 7
+#define VIEW_FEEDBACK 1 << 8
+
+#endif
---
+++ restaurant_permissions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "permissions.h"
+
+int main()
+{
+    int userPermissions;
+    printf("\n*** Welcome to The Restaurant Of Permissions ***\n\n");
+    printf("Please type in your permissions id: ");
+    scanf("%d", &userPermissions);
+    return 0;
+}
