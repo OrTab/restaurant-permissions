92d34dafe1b684bbf16bc60414609d0f6e1c7e44
OrTab <or.1532.tabin@gmail.com>
2023-12-10 04:15:31 +0200

    Added permissions and roles header , implement permission logic and added basic test cases

--- permissions.h
+++ permissions.h
@@ -1,13 +1,21 @@
 #ifndef PERMISSIONS
 
 #define READ_MENU 1 << 0
-#define EDIT_MENU 1 << 1
-#define PLACE_ORDERS 1 << 2
-#define MANAGE_RESERVATIONS 1 << 3
-#define VIEW_SALES_REPORTS 1 << 4
-#define PROCESS_PAYMENTS 1 << 5
-#define ACCESS_KITCHEN 1 << 6
-#define EMPLOYEE_MANAGEMENT 1 << 7
-#define VIEW_FEEDBACK 1 << 8
+#define GETTING_ORDERS 1 << 1
+#define EDIT_MENU 1 << 2
+#define PLACE_ORDERS 1 << 3
+#define MANAGE_RESERVATIONS 1 << 4
+#define VIEW_SALES_REPORTS 1 << 5
+#define PROCESS_PAYMENTS 1 << 6
+#define ACCESS_KITCHEN 1 << 7
+#define EMPLOYEE_MANAGEMENT 1 << 8
+#define VIEW_FEEDBACK 1 << 9
+
+#define TOTAL_PERMISSIONS (                \
+    (READ_MENU | EDIT_MENU) |              \
+    (PLACE_ORDERS | MANAGE_RESERVATIONS) | \
+    (VIEW_SALES_REPORTS) |                 \
+    (PROCESS_PAYMENTS | ACCESS_KITCHEN) |  \
+    (EMPLOYEE_MANAGEMENT | VIEW_FEEDBACK))
 
 #endif
--- restaurant_permissions.c
+++ restaurant_permissions.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "permissions.h"
+#include "roles.h"
+
+bool hasPermission(int userPermissions, int permissionValue)
+{
+    return (userPermissions & permissionValue) != 0;
+}
 
 int main()
 {
-    int userPermissions;
+    int userPermissionsInput;
     printf("\n*** Welcome to The Restaurant Of Permissions ***\n\n");
     printf("Please type in your permissions id: ");
-    scanf("%d", &userPermissions);
+    scanf("%d", &userPermissionsInput);
+    if (userPermissionsInput <= 0)
+    {
+        printf("Invalid input, must be a positive number");
+        return 0;
+    }
+    unsigned int userPermissions = (unsigned)userPermissionsInput;
+    if (userPermissions > TOTAL_PERMISSIONS)
+    {
+        printf("Invalid input, maximum value is %d", TOTAL_PERMISSIONS);
+        return 0;
+    }
+
+    bool isOwner = userPermissions == OWNER_ROLE;
+    bool canProcessPayments = hasPermission(userPermissions, PROCESS_PAYMENTS);
+    bool isWaiter = hasPermission(userPermissions, WAITER_ROLE);
+
+    printf("Can process payments: %d\n", canProcessPayments);
+    printf("Is waiter: %d\n", isWaiter);
+
+    if (isWaiter)
+    {
+        if (hasPermission(userPermissions, VIEW_SALES_REPORTS))
+        {
+            printf("Should never reach here");
+        }
+        else
+        {
+            printf("Waiter shouldn\'t view sales report");
+        }
+    }
+
     return 0;
 }
